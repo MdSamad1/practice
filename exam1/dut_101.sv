355241e4e596dee6bcf2528d46e0e2ae99c010ac
MdSamad1 <abdullah.samad@dsinnovators.com>
2024-06-08 22:47:17 +0600

    updating .gitignore file

--- exam1/dut_101.sv
+++ exam1/dut_101.sv
@@ -3,7 +3,7 @@ module dut_101 (
   output logic       out    // odd parity out
 );
 
-	logic [7:0] result;
-  	assign result = data[0] ^ data[1] ^ data[2] ^ data[3]^ data[4] ^ data[5] ^ data[6]^ data[7];
-	assign out = result;
+	//logic [7:0] result;
+  	assign out = data[0] ^ data[1] ^ data[2] ^ data[3]^ data[4] ^ data[5] ^ data[6]^ data[7];
+	//assign out = result;
 endmodule
